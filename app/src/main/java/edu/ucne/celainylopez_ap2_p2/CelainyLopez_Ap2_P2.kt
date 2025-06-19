0f17435505f09c50d1df0a5858d533aa573e035a
Celainy-Lopez <celainy_lopez@ucne.edu.do>
2025-06-18 20:18:40 -0400

    Configurando hilt

--- app/src/main/java/edu/ucne/celainylopez_ap2_p2/CelainyLopez_Ap2_P2.kt
+++
@@ -1,4 +0,0 @@
-package edu.ucne.celainylopez_ap2_p2
-
-class CelainyLopez_Ap2_P2 {
-}
---
+++ app/src/main/java/edu/ucne/celainylopez_ap2_p2/CelainyLopez_Ap2_P2App.kt
@@ -0,0 +1,8 @@
+package edu.ucne.celainylopez_ap2_p2
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class CelainyLopez_Ap2_P2App: Application() {
+}
