c91501a06be822b87a47aac5239e110392528fb7
Celainy-Lopez <celainy_lopez@ucne.edu.do>
2025-06-22 16:51:07 -0400

    Creando manejo de interfaz

--- app/src/main/java/edu/ucne/celainylopez_ap2_p2/presentation/Repositorys/RepositoryUiState.kt
+++ app/src/main/java/edu/ucne/celainylopez_ap2_p2/presentation/Repositorys/RepositoryUiState.kt
@@ -1,4 +1,11 @@
 package edu.ucne.celainylopez_ap2_p2.presentation.Repositorys
 
-class RepositoryUiState {
-}
+import edu.ucne.celainylopez_ap2_p2.data.remote.dto.RepositoryDto
+
+data class RepositoryUiState (
+    val name: String = "",
+    val descripcion: String = "",
+    val html_url: String = "",
+    val isLoading: Boolean = false,
+    val repository: List<RepositoryDto> = emptyList()
+)
