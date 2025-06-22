4b0fbf9ec5a208cb1480085f7a7b93e0c1748e63
Celainy-Lopez <celainy_lopez@ucne.edu.do>
2025-06-22 18:51:56 -0400

    Agregando campo

--- app/src/main/java/edu/ucne/celainylopez_ap2_p2/MainActivity.kt
+++ app/src/main/java/edu/ucne/celainylopez_ap2_p2/MainActivity.kt
@@ -24,7 +24,6 @@ class MainActivity : ComponentActivity() {
                 RepositoryListScreen(
                     drawerState = drawerState,
                     scope = scope
-
                 )
             }
         }
