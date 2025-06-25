93380953feb6b48b54b263eca122211b5359c0eb
Celainy-Lopez <celainy_lopez@ucne.edu.do>
2025-06-25 12:30:39 -0400

    Creando eventos del repository

--- app/src/main/java/edu/ucne/celainylopez_ap2_p2/presentation/Repositorys/RepositoryEvent.kt
+++ app/src/main/java/edu/ucne/celainylopez_ap2_p2/presentation/Repositorys/RepositoryEvent.kt
@@ -1,4 +1,12 @@
 package edu.ucne.celainylopez_ap2_p2.presentation.Repositorys
 
-interface RepositoryEvent {
+sealed interface RepositoryEvent {
+
+    data object GetRepositories: RepositoryEvent
+
+    data object PostRepository: RepositoryEvent
+
+    data object PutRepositories: RepositoryEvent
+
+    data  object DeleteRepositories: RepositoryEvent
 }
