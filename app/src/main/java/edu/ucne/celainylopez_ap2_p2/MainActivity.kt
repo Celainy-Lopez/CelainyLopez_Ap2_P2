f7733a30bd5eb231cc6df8098df3789fd153400c
Celainy-Lopez <celainy_lopez@ucne.edu.do>
2025-06-25 14:13:22 -0400

    Actualizando MainActivity

--- app/src/main/java/edu/ucne/celainylopez_ap2_p2/MainActivity.kt
+++ app/src/main/java/edu/ucne/celainylopez_ap2_p2/MainActivity.kt
@@ -4,11 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.material3.DrawerValue
-import androidx.compose.material3.rememberDrawerState
-import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.ui.Modifier
+import androidx.navigation.compose.rememberNavController
 import dagger.hilt.android.AndroidEntryPoint
-import edu.ucne.celainylopez_ap2_p2.presentation.Repositorys.RepositoryListScreen
+import edu.ucne.celainylopez_ap2_p2.presentation.navigation.HomeNavHost
 import edu.ucne.celainylopez_ap2_p2.ui.theme.CelainyLopez_Ap2_P2Theme
 
 @AndroidEntryPoint
@@ -18,13 +21,16 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             CelainyLopez_Ap2_P2Theme {
-                val drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
-                val scope = rememberCoroutineScope()
-
-                RepositoryListScreen(
-                    drawerState = drawerState,
-                    scope = scope
-                )
+                val navigation = rememberNavController()
+                Scaffold(
+                    modifier = Modifier.fillMaxSize()
+                ){ innerPadding ->
+                    Box(
+                        modifier = Modifier.padding(innerPadding)
+                    ){
+                        HomeNavHost(navigation)
+                    }
+                }
             }
         }
     }
