4850702a83bdd7ff9f146841b692f232fa855997
Celainy-Lopez <celainy_lopez@ucne.edu.do>
2025-07-09 18:53:13 -0400

    Listo para iniciar Segundo Parcial

--- app/src/main/java/edu/ucne/celainylopez_ap2_p2/MainActivity.kt
+++ app/src/main/java/edu/ucne/celainylopez_ap2_p2/MainActivity.kt
@@ -29,6 +29,7 @@ class MainActivity : ComponentActivity() {
                         modifier = Modifier.padding(innerPadding)
                     ){
                         HomeNavHost(navigation)
+
                     }
                 }
             }
