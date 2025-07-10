b63046c22e6b62b0570b0bc8a753f33c2b62d64b
Celainy-Lopez <celainy_lopez@ucne.edu.do>
2025-07-10 18:47:35 -0400

    Funcion agregada

--- app/src/main/java/edu/ucne/celainylopez_ap2_p2/data/remote/RepositoryinApi.kt
+++ app/src/main/java/edu/ucne/celainylopez_ap2_p2/data/remote/RepositoryinApi.kt
@@ -1,5 +1,6 @@
 package edu.ucne.celainylopez_ap2_p2.data.remote
 
+import edu.ucne.celainylopez_ap2_p2.data.remote.dto.ContribuidorDto
 import edu.ucne.celainylopez_ap2_p2.data.remote.dto.RepositoryDto
 import retrofit2.http.GET
 import retrofit2.http.Path
@@ -7,4 +8,10 @@ import retrofit2.http.Path
 interface GitHubApi {
     @GET("users/{username}/repos")
    suspend fun listRepos(@Path("username") username: String): List<RepositoryDto>
+
+    @GET("repos/{owner}/{repo}/contributors")
+    suspend fun listContributors(
+        @Path("owner") owner: String,
+        @Path("repo") repo: String
+    ): List<ContribuidorDto>
 }
