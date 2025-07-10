c36efc81b4f54e645d4cb48a0481970f8fd63e19
Celainy-Lopez <celainy_lopez@ucne.edu.do>
2025-07-10 19:05:46 -0400

    UiState Creado

--- app/src/main/java/edu/ucne/celainylopez_ap2_p2/presentation/contributors/ContributorUiState.kt
+++ app/src/main/java/edu/ucne/celainylopez_ap2_p2/presentation/contributors/ContributorUiState.kt
@@ -1,4 +1,13 @@
 package edu.ucne.celainylopez_ap2_p2.presentation.contributors
 
-class ContributorUiState {
-}
+import edu.ucne.celainylopez_ap2_p2.data.remote.dto.ContribuidorDto
+
+data class ContributorUiState(
+    val login: String = "",
+    val id: Int = 0,
+    val contribution: Int = 0,
+    val type: String = "",
+    val isLoading: Boolean = false,
+    val contributor: List<ContribuidorDto> = emptyList(),
+    val errorMessage: String? = null,
+)
