07c9c5c8c0d2392108c785198c965e7f939da5f0
Celainy-Lopez <celainy_lopez@ucne.edu.do>
2025-07-10 19:13:37 -0400

    ViewModel de contribuidores creado

--- app/src/main/java/edu/ucne/celainylopez_ap2_p2/presentation/contributors/ContributorViewModel.kt
+++ app/src/main/java/edu/ucne/celainylopez_ap2_p2/presentation/contributors/ContributorViewModel.kt
@@ -1,4 +1,63 @@
 package edu.ucne.celainylopez_ap2_p2.presentation.contributors
 
-class ContributorViewModel {
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
+import edu.ucne.celainylopez_ap2_p2.data.remote.Resource
+import edu.ucne.celainylopez_ap2_p2.data.repository.RepositoryRepository
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class ContributorViewModel @Inject constructor(
+    private val repositoryRepository: RepositoryRepository
+) : ViewModel() {
+
+    private val _uiState = MutableStateFlow(ContributorUiState())
+    val uiState = _uiState.asStateFlow()
+
+    private val _loading = MutableStateFlow(false)
+    val loading: StateFlow<Boolean> = _loading
+
+
+    fun onEvent(event: ContributorEvent) {
+        when (event) {
+            is ContributorEvent.GetContributors -> getContributor(event.repoPath)
+        }
+    }
+
+    private fun getContributor(repos: String) {
+        val (owner, repo) = repos.split("/")
+        viewModelScope.launch {
+            repositoryRepository.getContributors(owner, repo).collectLatest { resource ->
+                when (resource) {
+                    is Resource.Loading -> {
+                        _uiState.update { it.copy(isLoading = true) }
+                    }
+                    is Resource.Success -> {
+                        _uiState.update {
+                            it.copy(
+                                contributor = resource.data ?: emptyList(),
+                                isLoading = false,
+                                errorMessage = null
+                            )
+                        }
+                    }
+                    is Resource.Error -> {
+                        _uiState.update {
+                            it.copy(
+                                isLoading = false,
+                                errorMessage = resource.message
+                            )
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
