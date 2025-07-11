3cb9b08948a43d3e56374fb69b3765a252d6e889
Celainy-Lopez <celainy_lopez@ucne.edu.do>
2025-07-10 20:01:25 -0400

    Serializable agregado

--- app/src/main/java/edu/ucne/celainylopez_ap2_p2/presentation/navigation/Screen.kt
+++ app/src/main/java/edu/ucne/celainylopez_ap2_p2/presentation/navigation/Screen.kt
@@ -2,11 +2,13 @@ package edu.ucne.celainylopez_ap2_p2.presentation.navigation
 
 import kotlinx.serialization.Serializable
 
-sealed class Screen {
+sealed class Screen{
     @Serializable
     data class Repository(val Id: Int?): Screen()
 
     @Serializable
-    data object RepositoryList:Screen()
+    data object RepositoryList: Screen()
 
+    @Serializable
+    data class ContributorList(val owner: String, val repo: String) : Screen()
 }
